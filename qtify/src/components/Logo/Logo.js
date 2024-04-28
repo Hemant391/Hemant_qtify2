9922596da678b43d0639a62f6026fd9037cb8491
Hemant391 <148564538+Hemant391@users.noreply.github.com>
2024-04-28 17:23:48 +0530

    Nav 3

--- qtify/src/components/Logo/Logo.js
+++ qtify/src/components/Logo/Logo.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import logoimage from "../../assets/logo.png"
-
+import styles from '../Logo/Logo.module.css'
 
  function Logo(){
     return(
         <>
+        <div className={styles.logo}>
+
          <img src={logoimage} alt='logo' width={67} />
+        </div>
         </>
     )
 }
